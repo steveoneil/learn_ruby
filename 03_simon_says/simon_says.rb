96b53937dc9c553ca60e400000dcfe603431e59b
steveoneil <steve@steveoneil.com>
2016-01-18 22:10:59 -0500

    Simon Says Passing

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -20,5 +20,31 @@ def start_of_word(input_text, num)
   input_text[0, num]
 end
 
+def first_word(input_text)
+  input_text.split.first
+end
 
-litte_words = ["a", "an", "the", "at", "by", "for", "in", "of", "on", "to", "up", "and", "as", "but", "or", "nor", "over"]
+def titleize(input_text)
+
+  output_text = input_text.split
+  output_text.each_with_index do |word, i|
+    output_text[i] = word.capitalize
+    end
+
+  little_words = { "A": "a", "An": "an", "The": "the", "At": "at", "By": "by",
+                   "For": "for", "In": "in", "Of": "of", "On": "on", "To": "to",
+                   "Up": "up", "And": "and", "As": "as", "But": "but",
+                   "Or": "or", "Nor": "nor", "Over": "over" }
+  output_text.each_with_index do |word, i|
+    if i >= 1
+      little_words.each do |cap, no_cap|
+        if word == cap.to_s
+          output_text[i] = no_cap
+        end
+      end
+    end
+  end
+
+  output_text.join(" ")
+
+end
