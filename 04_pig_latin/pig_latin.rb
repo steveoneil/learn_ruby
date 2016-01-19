a5402911673f5d60388a7fbe98d28d197b16a455
steveoneil <steve@steveoneil.com>
2016-01-18 23:38:36 -0500

    Pig Latin Translate Method 4 Failures remaining

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,10 +1,17 @@
 def translate(english)
   consonants = "bcdfghjklmnpqrstvwxyz".chars
   pig_latin = english.chars
-  if consonants.include?(new_text[0])
-    # first_letter = new_text[0]
+  if consonants.include?(pig_latin[0])
     first_letter = pig_latin.shift
     pig_latin << first_letter
+    if consonants.include?(pig_latin[0])
+      first_letter = pig_latin.shift
+      pig_latin << first_letter
+      if consonants.include?(pig_latin[0])
+        first_letter = pig_latin.shift
+        pig_latin << first_letter
+      end
+    end
   end
   pig_latin << "a"
   pig_latin << "y"
